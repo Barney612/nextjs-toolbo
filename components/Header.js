861cb24285ba8c2fbc5b94460a431c07bc25b8c2
Barney612 <148951477+Barney612@users.noreply.github.com>
2023-10-27 00:49:13 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,5 @@
 export default function Header({ title }) {
-  return (
-    <>
-      <h1 className="title">{title}</h1>
-      <p>
-        Hi 👋! This Isaiah gives you an amazing product thats just right for you
-          Netlify Functions
-        </a>
-        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
+ 
 
       <p>
         You can find the code for this project on GitHub at{" "}
